f25ca9b7aab7090ceabe962e8c5a540707fd9e4a
Shan Bollapragada <sbollap1@gmail.com>
2016-11-04 14:15:34 -0400

    testing git

---
+++ _Rough1.java
@@ -0,0 +1,15 @@
+public class _Rough1 {
+
+  public static void main(String[] args) {
+      String s1 = "Jadva";
+      String s2 = "Java";
+    
+    
+    System.out.println(s1 == s2);
+    
+    System.out.println(s1.equals(s2)); 
+
+  }
+}
+      
+  
